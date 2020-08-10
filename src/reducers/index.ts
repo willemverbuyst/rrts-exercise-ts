37dd7411662c669273d6234229dc6a44fe126955
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-10 19:17:30 +0200

    Wire up deleteTodo action

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { todosReducer } from './todos';
-import { Todo } from '../actions';
+import { Todo } from '../actions/todos';
 
 export interface StoreState {
   todos: Todo[];
